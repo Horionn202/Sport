b36293965146b1bfa3e55559cca232b5d5ba8406
JeremyG08 <73029905@certus.edu.pe>
2025-10-14 22:52:15 -0500

    +

--- src/main/java/com/example/Sport/controllers/usuarioController.java
+++ src/main/java/com/example/Sport/controllers/usuarioController.java
@@ -4,9 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class usuarioController {
+public class UsuarioController {
+
     @GetMapping("/")
-    public String verLogin(){
+    public String verDashboard() {
         return "DasboardPrincipal";
+    }
+
+    @GetMapping("/login")
+    public String verLogin() {
+        return "IniciarSeccion";
     }
 }
